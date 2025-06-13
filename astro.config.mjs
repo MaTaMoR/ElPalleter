d9e6ce41146bdb592e4108d308f1a8fc097515e5
MaTaMoR <matamor98@hotmail.com>
2025-06-14 00:48:47 +0200

    i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:4321',
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en', 'ca'],
     routing: {
-      prefixDefaultLocale: false
+      prefixDefaultLocale: false,
+      fallbackType: "rewrite"
     }
-  },
-  // Tu configuración existente...
+  }
 });
