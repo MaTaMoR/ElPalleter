cfc01493db2a13b8e3636893004b7be511586d28
MaTaMoR <matamor98@hotmail.com>
2025-11-23 23:06:40 +0100

    updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import node from '@astrojs/node';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  output: 'hybrid', 
-  adapter: node({
-    mode: 'standalone'
-  }),
-  integrations: [react()],
+  output: 'static',
   vite: {
     resolve: {
       alias: {
